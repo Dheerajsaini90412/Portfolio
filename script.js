acf9da8ac6379df2eb8072f4cc8a3e25cbd7d9f7
Dheerajsaini90412 <dheerajsaini90412@gmail.com>
2024-05-09 16:22:06 +0530

    contact-form

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const form=document.querySelector('form');
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbwU7vt2UoG3kRH954Z_onpkV237NqNDG7v5v-s03D_P1Yy81EL6yoMcGub5dul03U5w/exec'
 
-function sendEmail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "dheerajsaini0412@gmail.com",
-        Password : "E630BAF9CC9F34F78FEE31CAB7048C56C4D6",
-        To : 'dheerajsaini90412@gmail.com',
-        From : "dheerajsaini90412@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );    
-}
+// const form = document.forms['contact-us']
+    
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => console.log('Success!', response))
+//     .catch(error => console.error('Error!', error.message))
+// })
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwU7vt2UoG3kRH954Z_onpkV237NqNDG7v5v-s03D_P1Yy81EL6yoMcGub5dul03U5w/exec'
+const form = document.forms['contact-us'];
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your response is submitted successfully." ))
+  .then(() => {  window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
